4ce11d053f8c984b759bf45f1a68649318f1f25f
Broadsword117 <antony.rc.kathage@gmail.com>
2019-01-25 00:45:39 +1000

    file rename

--- linear_pot/linear_pot.ino
+++ linear_pot/linear_pot.ino
@@ -14,8 +14,7 @@
  */
 
 int potPin = 2;    // select the input pin for the potentiometer
-int ledPin = 13;   // select the pin for the LED
-int val = 0;       // variable to store the value coming from the sensor
+int val;      // variable to store the value coming from the sensor
 
 void setup() {
   pinMode(ledPin, OUTPUT);  // declare the ledPin as an OUTPUT
